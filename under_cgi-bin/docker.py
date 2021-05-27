82a4edf7fede4fa7ac3f880bfc904497ad0a37f4
sarthakSharma5 <64486079+sarthakSharma5@users.noreply.github.com>
2021-05-27 11:58:40 +0530

    update multi-threading

--- under_cgi-bin/docker.py
+++ under_cgi-bin/docker.py
@@ -2,31 +2,35 @@
 
 import subprocess
 import cgi
+import threading
 
-print("content-type: text/html")
-print()
+def reply():
+    print("content-type: text/html")
+    print()
 
-form = cgi.FieldStorage()
-ctname = form.getvalue("cnm")
-ctimg = form.getvalue("dimg")
+    form = cgi.FieldStorage()
+    ctname = form.getvalue("cnm")
+    ctimg = form.getvalue("dimg")
 
-cmd = "sudo docker run -dit --name {0} {1}".format(ctname, ctimg)
-check = "sudo docker ps | grep {}".format(ctname)
-colhead = "sudo docker ps | grep IMAGE"
+    cmd = "sudo docker run -dit --name {0} {1}".format(ctname, ctimg)
+    check = "sudo docker ps | grep {}".format(ctname)
+    colhead = "sudo docker ps | grep IMAGE"
 
-print("Starting container: {0} , with image: {1} | \n".format(ctname, ctimg))
-output = subprocess.getstatusoutput(cmd)
+    print("Starting container: {0} , with image: {1} | \n".format(ctname, ctimg))
+    output = subprocess.getstatusoutput(cmd)
 
-print("status-code: {} | \n".format(output[0]))
-print("output: <PRE> {} </PRE> \n".format(output[1]))
+    print("status-code: {} | \n".format(output[0]))
+    print("output: <PRE> {} </PRE> \n".format(output[1]))
 
-checkout = subprocess.getstatusoutput(check)
-setcolhead= subprocess.getstatusoutput(colhead)
+    checkout = subprocess.getstatusoutput(check)
+    setcolhead= subprocess.getstatusoutput(colhead)
 
-if checkout[0] == 0:
-    print("Container Up & Running \n")
-print("<PRE> {} </PRE> \n".format(setcolhead[1]))
-print("<PRE> {} </PRE> \n".format(checkout[1]))
+    if checkout[0] == 0:
+        print("Container Up & Running \n")
+    print("<PRE> {} </PRE> \n".format(setcolhead[1]))
+    print("<PRE> {} </PRE> \n".format(checkout[1]))
 
-print('Use this link to work with container: http://192.168.99.103/forms/cont.html')
+    print('Use this link to work with container: http://192.168.99.103/forms/cont.html')
 
+rep=threading.Thread( target=reply )
+rep.start()
