886722272b45ec1d69f7ca94e93e6237997b678c
sarthakSharma5 <64486079+sarthakSharma5@users.noreply.github.com>
2021-05-27 12:03:08 +0530

    update multi-threading

--- under_cgi-bin/cont.py
+++ under_cgi-bin/cont.py
@@ -2,24 +2,28 @@
 
 import subprocess
 import cgi
+import threading
 
-print("content-type: text/html")
-print()
+def reply():
+    print("content-type: text/html")
+    print()
 
-form = cgi.FieldStorage()
-ctname = form.getvalue("cnm")
-ctcmd = form.getvalue("cmd")
+    form = cgi.FieldStorage()
+    ctname = form.getvalue("cnm")
+    ctcmd = form.getvalue("cmd")
 
-start = "sudo docker start {}".format(ctname)
-execmd = "sudo docker exec {0} {1}".format(ctname, ctcmd)
+    start = "sudo docker start {}".format(ctname)
+    execmd = "sudo docker exec {0} {1}".format(ctname, ctcmd)
 
-startcont = subprocess.getstatusoutput(start)
-if startcont[0] == 0:
-    print("Container {} up & running | \n".format(ctname))
+    startcont = subprocess.getstatusoutput(start)
+    if startcont[0] == 0:
+        print("Container {} up & running | \n".format(ctname))
 
-output = subprocess.getstatusoutput(execmd)
-print("status-code: {} | \n".format(output[0]))
-#if startcont[0] == 126:
-#    print("Container donot have required software or command")
-print("output: <PRE> {} </PRE> \n".format(output[1]))
+    output = subprocess.getstatusoutput(execmd)
+    print("status-code: {} | \n".format(output[0]))
+    #if startcont[0] == 126:
+    #    print("Container donot have required software or command")
+    print("output: <PRE> {} </PRE> \n".format(output[1]))
 
+rep=threading.Thread( target=reply )
+rep.start()
