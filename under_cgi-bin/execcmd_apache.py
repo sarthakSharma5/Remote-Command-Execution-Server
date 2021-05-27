10566dea751ae75698255c7556ea89bc1fe29f2d
sarthakSharma5 <64486079+sarthakSharma5@users.noreply.github.com>
2021-05-27 11:55:54 +0530

    update multi-threading

--- under_cgi-bin/execcmd_apache.py
+++ under_cgi-bin/execcmd_apache.py
@@ -3,27 +3,30 @@
 import subprocess
 import cgi
 
-print("content-type: text/html")
-print()
+def reply():
+    print("content-type: text/html")
+    print()
 
-form = cgi.FieldStorage()
-cmd = form.getvalue("cmd")
+    form = cgi.FieldStorage()
+    cmd = form.getvalue("cmd")
 
-if 'sudo' in cmd:
-    cmd = cmd.replace('sudo', '')
-    print("Cannot allow use of sudo for general users")
+    if 'sudo' in cmd:
+        cmd = cmd.replace('sudo', '')
+        print("Cannot allow use of sudo for general users")
 
-if 'docker ' in cmd:
-    print("You are not authorized to use docker-ce!")
-    exit()
+    if 'docker ' in cmd:
+        print("You are not authorized to use docker-ce!")
+        exit()
 
-print("command: {} | ".format(cmd))
-output = subprocess.getstatusoutput(cmd)
+    print("command: {} | ".format(cmd))
+    output = subprocess.getstatusoutput(cmd)
 
-print("status-code: {} | ".format(output[0]))
-print()
-print("output: <PRE> {} </PRE> \n".format(output[1]))
-print("<BR>")
+    print("status-code: {} | ".format(output[0]))
+    print()
+    print("output: <PRE> {} </PRE> \n".format(output[1]))
+    print("<BR>")
 
-print()
+    print()
 
+rep = threading.Thread( target=reply )
+rep.start()
